49fb43327b090b41da600b8c9eb98382471c128f
Felix Arntz <felix.arntz@vercel.com>
2026-05-05 14:24:24 -0500

    add konsistent-convention emit CLI so that packages don't have to implement their own script

--- packages/convention/tsup.config.ts
+++ packages/convention/tsup.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  outDir: "dist",
-  clean: true,
-  splitting: false,
-  dts: true,
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["esm"],
+    outDir: "dist",
+    clean: true,
+    splitting: false,
+    dts: true,
+  },
+  {
+    entry: ["src/cli.ts"],
+    format: ["esm"],
+    outDir: "dist",
+    clean: false,
+    splitting: false,
+    dts: false,
+    banner: { js: "#!/usr/bin/env node" },
+  },
+]);
